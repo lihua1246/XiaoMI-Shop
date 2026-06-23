92bf90cc4160151e41552d0b358c3ee870ee24ac
yz931 <yz931@example.com>
2026-06-23 10:04:00 +0800

    精简购物车js

--- js/cart.js
+++ js/cart.js
@@ -1,148 +1,58 @@
-var cartKey = 'xiaomiCart';
-
-function getCartList() {
-    var text = localStorage.getItem(cartKey);
-    if (text) {
-        return JSON.parse(text);
-    }
-    return [];
-}
-
-function saveCartList(list) {
-    localStorage.setItem(cartKey, JSON.stringify(list));
-}
-
-function addProductToCart(btn) {
-    var list = getCartList();
-    var id = btn.getAttribute('data-id');
-    var hasProduct = false;
-
-    for (var i = 0; i < list.length; i++) {
-        if (list[i].id === id) {
-            list[i].num = list[i].num + 1;
-            hasProduct = true;
-        }
-    }
-
-    if (!hasProduct) {
-        list.push({
-            id: id,
-            name: btn.getAttribute('data-name'),
-            desc: btn.getAttribute('data-desc'),
-            price: Number(btn.getAttribute('data-price')),
-            image: btn.getAttribute('data-image'),
-            num: 1
-        });
-    }
-
-    saveCartList(list);
-}
-
-var addButtons = document.querySelectorAll('[data-cart-action="add"]');
-for (var i = 0; i < addButtons.length; i++) {
-    addButtons[i].onclick = function () {
-        addProductToCart(this);
-    };
-}
-
 var cartList = document.getElementById('cartList');
 var cartTotal = document.getElementById('cartTotal');
 var checkedCount = document.getElementById('checkedCount');
-var cartContent = document.getElementById('cartContent');
-var cartEmpty = document.getElementById('cartEmpty');
-
-function showCart() {
-    if (!cartList) {
-        return;
-    }
-
-    var list = getCartList();
-    var html = '';
-
-    if (list.length === 0) {
-        cartContent.style.display = 'none';
-        cartEmpty.style.display = 'block';
-        return;
-    }
-
-    cartContent.style.display = 'block';
-    cartEmpty.style.display = 'none';
-
-    for (var i = 0; i < list.length; i++) {
-        html += '<div class="cart-item" data-index="' + i + '">';
-        html += '<span class="cart-col-check"><input type="checkbox" class="cart-checkbox" checked></span>';
-        html += '<span class="cart-col-product">';
-        html += '<span class="cart-product-visual cart-product-visual-img"><img src="' + list[i].image + '" alt="' + list[i].name + '"></span>';
-        html += '<span class="cart-product-info">';
-        html += '<strong class="cart-product-name">' + list[i].name + '</strong>';
-        html += '<span class="cart-product-desc">' + list[i].desc + '</span>';
-        html += '</span></span>';
-        html += '<span class="cart-col-price"><span class="cart-price">¥' + list[i].price + '</span></span>';
-        html += '<span class="cart-col-qty">';
-        html += '<span class="cart-qty-stepper">';
-        html += '<button type="button" class="qty-btn minus-btn">-</button>';
-        html += '<input type="text" class="qty-input" value="' + list[i].num + '" readonly>';
-        html += '<button type="button" class="qty-btn plus-btn">+</button>';
-        html += '</span></span>';
-        html += '<span class="cart-col-subtotal"><span class="cart-subtotal">¥' + (list[i].price * list[i].num) + '</span></span>';
-        html += '</div>';
-    }
-
-    cartList.innerHTML = html;
-    countTotal();
-    bindCartButtons();
-}
 
 function countTotal() {
-    var list = getCartList();
+    var items = document.querySelectorAll('.cart-item');
     var total = 0;
     var count = 0;
 
-    for (var i = 0; i < list.length; i++) {
-        total += list[i].price * list[i].num;
-        count += list[i].num;
+    for (var i = 0; i < items.length; i++) {
+        var price = Number(items[i].getAttribute('data-price'));
+        var input = items[i].querySelector('.qty-input');
+        var subtotal = items[i].querySelector('.cart-subtotal');
+        var num = Number(input.value);
+
+        subtotal.innerText = '¥' + price * num;
+        total += price * num;
+        count += num;
     }
 
-    if (cartTotal) {
-        cartTotal.innerText = '¥' + total;
-    }
-    if (checkedCount) {
-        checkedCount.innerText = count;
-    }
+    cartTotal.innerText = '¥' + total;
+    checkedCount.innerText = count;
 }
 
-function bindCartButtons() {
+function bindButtons() {
     var plusButtons = document.querySelectorAll('.plus-btn');
     var minusButtons = document.querySelectorAll('.minus-btn');
 
     for (var i = 0; i < plusButtons.length; i++) {
         plusButtons[i].onclick = function () {
-            var index = this.parentNode.parentNode.parentNode.getAttribute('data-index');
-            var list = getCartList();
-            list[index].num = list[index].num + 1;
-            saveCartList(list);
-            showCart();
+            var input = this.parentNode.querySelector('.qty-input');
+            input.value = Number(input.value) + 1;
+            countTotal();
         };
     }
 
     for (var j = 0; j < minusButtons.length; j++) {
         minusButtons[j].onclick = function () {
-            var index = this.parentNode.parentNode.parentNode.getAttribute('data-index');
-            var list = getCartList();
-            if (list[index].num > 1) {
-                list[index].num = list[index].num - 1;
+            var input = this.parentNode.querySelector('.qty-input');
+            if (Number(input.value) > 1) {
+                input.value = Number(input.value) - 1;
             }
-            saveCartList(list);
-            showCart();
+            countTotal();
         };
     }
 }
 
 var checkoutBtn = document.getElementById('checkoutBtn');
+if (cartList) {
+    bindButtons();
+    countTotal();
+}
+
 if (checkoutBtn) {
     checkoutBtn.onclick = function () {
         alert('当前合计：' + cartTotal.innerText);
     };
 }
-
-showCart();
